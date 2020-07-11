a40aea773bf724514ef089fa12bc3af63da705d4
Manabu Seki <github@zitaku.net>
2020-07-12 00:22:45 +0900

    change log output

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 )
 
 func main() {
-	log.Infoln("pr_auto_assign version %s", version)
+	log.Infof("pr_auto_assign version %s", version)
 	var errCount int
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
@@ -24,33 +24,33 @@ func main() {
 		errCount++
 	}
 
+	own := os.Getenv("CI_REPO_OWNER")
+	if own == "" {
+		log.Errorf("CI_REPO_OWNER is missing")
+		errCount++
+	}
+	repo := os.Getenv("CI_REPO_NAME")
+	if repo == "" {
+		log.Errorf("CI_REPO_NAME is missing")
+		errCount++
+	}
+	configPath := os.Getenv("CI_CONFIG_PATH")
+	if configPath == "" {
+		log.Errorf("CI_CONFIG_PATH is missing")
+		errCount++
+	}
 	tmpID := os.Getenv("CI_PULL_REQUEST_ID")
 	if tmpID == "" {
-		log.Error("CI_PULL_REQUEST_ID is missing")
+		log.Errorf("CI_PULL_REQUEST_ID is missing")
 		errCount++
 	}
 	prID, err := strconv.ParseInt(tmpID, 10, 64)
 	if err != nil {
 		panic(err)
 	}
-	own := os.Getenv("CI_REPO_OWNER")
-	if own == "" {
-		log.Error("CI_REPO_OWNER is missing")
-		errCount++
-	}
-	repo := os.Getenv("CI_REPO_NAME")
-	if repo == "" {
-		log.Error("CI_REPO_NAME is missing")
-		errCount++
-	}
-	configPath := os.Getenv("CI_CONFIG_PATH")
-	if configPath == "" {
-		log.Error("CI_CONFIG_PATH is missing")
-		errCount++
-	}
 
 	if errCount != 0 {
-		log.Errorf("failed to initialize action, error count: %d", errCount)
+		log.Error("failed to initialize action, error count: %d", errCount)
 	}
 
 	ctx := context.Background()
